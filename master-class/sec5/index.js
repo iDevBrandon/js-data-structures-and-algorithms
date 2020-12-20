2e655be3f4b527c86fe6817c66bc6fbdeaf5c367
Brandon Ha <hsy0602@outlook.com>
2020-12-21 00:26:46 +0900

    22. Step 4: Solve or simplify

--- master-class/sec5/index.js
+++ master-class/sec5/index.js
@@ -1,3 +1,23 @@
 function reverseArray(a) {
   return a.reverse();
 }
+
+// 22. Step 4: Solve or simplify
+function charCount(str) {
+  // make object to return in the end
+  let result = {};
+
+  // loop over strings
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    // if the char is text/number in object, add one to count
+    if (result[char] > 0) {
+      result[char]++;
+    } else {
+      // if the char is NOT text/number, set value to 1
+      result[char] = 1;
+    }
+  }
+  // return object at end
+  return result;
+}
