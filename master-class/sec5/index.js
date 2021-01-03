d37a5038f1409dfaaf3f4ab97ca4c85b4e3e5310
iDevBrandon <hsy0602@outlook.com>
2021-01-04 01:22:29 +0900

    update

--- master-class/sec5/index.js
+++ master-class/sec5/index.js
@@ -44,3 +44,31 @@ function same(arr1, arr2) {
   return true;
 }
 
+// 28.
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  console.log(lookup);
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+validAnagram("anagram", "nagaram");
