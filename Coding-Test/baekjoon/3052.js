934520e04c25fb9acbd7b9e08d6917bdf4da9e80
Brandon Ha <hsy0602@outlook.com>
2021-02-24 14:49:16 +0900

    1st

---
+++ Coding-Test/baekjoon/3052.js
@@ -0,0 +1,13 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const newArr = [];
+
+input.forEach((x) => {
+  const num = x % 42;
+
+  if (newArr.indexOf(num) === -1) {
+    newArr.push(num);
+  }
+});
+
+console.log(newArr.length);
